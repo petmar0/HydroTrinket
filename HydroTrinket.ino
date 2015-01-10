64f303523b417f278df566e1f2207ee2c04344db
petmar0 <petmar@gmail.com>
2015-01-10 07:58:40 -0500

    Initial Commit and Naming Changes

--- HydroTrinket.ino
+++ HydroTrinket.ino
@@ -1,31 +1,31 @@
 /*
-  Blink
-  Turns on an LED on for one second, then off for one second, repeatedly.
- 
-  This example code is in the public domain.
- */
- 
-// Pin 13 has an LED connected on most Arduino boards.
-// give it a name:
-int Valve = 1;
-int Pump = 2;
+  HydroTrinket
+  A hydroponics controller using the Adafruit Trinket.
+  
+  01/10/2015, Pete and Katie Marchetto
+  This code is beerware: if you use it and meet either or both of us, you owe us a beer. Otherwise, simply follow cc-by 2.0.
+*/
 
-// the setup routine runs once when you press reset:
-void setup() {                
-  // initialize the digital pin as an output.
-  pinMode(Valve, OUTPUT);
-  pinMode(Pump, OUTPUT);
-  Serial.begin(9600);
+int Valve = 1;  //Control the valve relay from pin 1.
+int Pump = 2;  //Control the pump relay from pin 2.
+
+int PumpIn=100;  //Amount of time to fill the tray in seconds.
+int WetDwell=3600;  //Amount of time to allow the hydroponics fluid to dwell in the tray in seconds.
+int ValveOut=100;  //Amount of time to drain the tray into the bucket in seconds.
+int DryDwell=3600;  //Amount of time to allow the plants to sit without hydroponics fluid in seconds.
+
+void setup() {
+  pinMode(Valve, OUTPUT);  //Set the valve relay pin as an output.
+  pinMode(Pump, OUTPUT);  //Set the pump relay pin as an output.
 }
 
-// the loop routine runs over and over again forever:
 void loop() {
-  digitalWrite(Valve, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);               // wait for a second
-  digitalWrite(Pump, HIGH);
-  delay(1000);
-  digitalWrite(Valve, LOW);
-  delay(1000);
-  digitalWrite(Pump, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);               // wait for a second
+  digitalWrite(Pump, HIGH);  //Turn on the pump relay.
+  delay(PumpIn*1000);  //Leave the pump relay on for PumpIn*1000 msec.
+  digitalWrite(Pump, LOW);  //Turn off the pump relay.
+  delay(WetDwell*1000);  //Leave the pump relay off for WetDwell*1000 msec.
+  digitalWrite(Valve, HIGH);  //Turn on the valve relay.
+  delay(ValveOut*1000);  //Leave the valve relay on for ValveOut*1000 msec.
+  digitalWrite(Valve, LOW);  //Turn off the valve relay.
+  delay(DryDwell*1000);  //Leave the valve relay off for DryDwell*1000 msec.
 }
